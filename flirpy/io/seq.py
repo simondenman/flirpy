d02d1429885a78c82a779476606ac86cf2eab4ef
Banan Suhr <b.suhr@qut.edu.au>
2021-10-01 02:38:25 +1000

    SEQ frame extraction improvements

--- flirpy/io/seq.py
+++ flirpy/io/seq.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2
 import os
@@ -11,27 +10,30 @@ import subprocess
 from tqdm.autonotebook import tqdm
 
 try:
-  from pathlib import Path
+    from pathlib import Path
 except ImportError:
-  from pathlib2 import Path  # python 2 backport
+    from pathlib2 import Path  # python 2 backport
 
 from flirpy.util.exiftool import Exiftool
 from flirpy.io.fff import Fff
 
 logger = logging.getLogger(__name__)
 
+
 class splitter:
-    
-    def __init__(self, output_folder="./", exiftool_path=None, start_index=0, step=1, width=640, height=512, split_folders=True, preview_format="jpg"):
-        
+
+    def __init__(self, output_folder="./", exiftool_path=None, start_index=0, step=1, width=640, height=512,
+                 split_folders=True, preview_format="jpg"):
+
         self.exiftool = Exiftool(exiftool_path)
-            
+
         self.width = width
         self.height = height
         self.start_index = start_index
         self.step = step
         self.frame_count = self.start_index
         self.export_tiff = True
+        self.export_fff = True
         self.export_meta = True
         self.export_preview = True
         self.export_radiometric = True
@@ -39,7 +41,7 @@ class splitter:
         self.split_folders = split_folders
         self.split_filetypes = True
         self.use_mmap = True
-        
+
         if preview_format in ["jpg", "jpeg", "png", "tiff"]:
             self.preview_format = preview_format
         else:
@@ -47,10 +49,10 @@ class splitter:
 
         self.output_folder = os.path.expanduser(output_folder)
         Path(self.output_folder).mkdir(exist_ok=True)
-    
+
     def set_start_index(self, index):
         self.start_index = int(index)
-        
+
     def process(self, file_list):
 
         if isinstance(file_list, str):
@@ -59,11 +61,11 @@ class splitter:
         file_list = [os.path.expanduser(f) for f in file_list]
 
         logger.info("Splitting {} files".format(len(file_list)))
-        
+
         self.frame_count = self.start_index
 
         folders = []
-        
+
         for seq in tqdm(file_list):
 
             if self.split_folders:
@@ -98,31 +100,33 @@ class splitter:
                 # Copy geotags
                 if self.export_tiff:
                     logger.info("Copying tags to radiometric")
-                    self.exiftool.copy_meta(folder, filemask=copy_filemask, output_folder=radiometric_folder, ext="tiff")
-                
+                    self.exiftool.copy_meta(folder, filemask=copy_filemask, output_folder=radiometric_folder,
+                                            ext="tiff")
+
                 if self.export_preview:
                     logger.info("Copying tags to preview")
-                    self.exiftool.copy_meta(folder, filemask=copy_filemask, output_folder=preview_folder, ext=self.preview_format)
-        
+                    self.exiftool.copy_meta(folder, filemask=copy_filemask, output_folder=preview_folder,
+                                            ext=self.preview_format)
+
         return folders
-        
+
     def _write_tiff(self, filename, data):
         logger.debug("Writing {}", filename)
         cv2.imwrite(filename, data.astype("uint16"))
 
     def _write_preview(self, filename, data):
-        drange = data.max()-data.min()
-        preview_data = 255.0*((data-data.min())/drange)
+        drange = data.max() - data.min()
+        preview_data = 255.0 * ((data - data.min()) / drange)
         logger.debug("Writing {}", filename)
         cv2.imwrite(filename, preview_data.astype('uint8'))
-            
+
     def _make_split_folders(self, output_folder):
         Path(os.path.join(output_folder, "raw")).mkdir(exist_ok=True)
         Path(os.path.join(output_folder, "radiometric")).mkdir(exist_ok=True)
         Path(os.path.join(output_folder, "preview")).mkdir(exist_ok=True)
 
     def _get_fff_iterator(self, seq_blob):
-        
+
         magic_pattern_fff = "\x46\x46\x46\x00".encode()
 
         valid = re.compile(magic_pattern_fff)
@@ -131,14 +135,14 @@ class splitter:
     def _check_overwrite(self, path):
         exists = os.path.exists(path)
         return (not exists) or (exists and self.overwrite)
-    
+
     def _process_seq(self, input_file, output_subfolder):
-        
+
         logger.debug("Processing {}".format(input_file))
         gpslog = open(os.path.join(output_subfolder, "gpsLog.txt"), 'w')
-        
+
         with open(input_file, 'rb') as seq_file:
-            
+
             # Memory mapping may speed up things. This is kinda slow though, because we still have to parse the entire file
             # and then go back through the regexes to find individual frames. Should really use a stream.
             if self.use_mmap:
@@ -150,37 +154,40 @@ class splitter:
 
             pos = []
             prev_pos = 0
-            
+
             meta = None
-            
+
             for i, match in tqdm(enumerate(it)):
                 index = match.start()
-                chunksize = index-prev_pos
+                chunksize = index - prev_pos
                 pos.append((index, chunksize))
                 prev_pos = index
-                
+
                 if self.split_filetypes:
                     self._make_split_folders(output_subfolder)
-                                     
+
                     filename_fff = os.path.join(output_subfolder, "raw", "frame_{0:06d}.fff".format(self.frame_count))
-                    filename_tiff = os.path.join(output_subfolder, "radiometric", "frame_{0:06d}.tiff".format(self.frame_count))
-                    filename_preview = os.path.join(output_subfolder, "preview", "frame_{:06d}.{}".format(self.frame_count, self.preview_format))
+                    filename_tiff = os.path.join(output_subfolder, "radiometric",
+                                                 "frame_{0:06d}.tiff".format(self.frame_count))
+                    filename_preview = os.path.join(output_subfolder, "preview",
+                                                    "frame_{:06d}.{}".format(self.frame_count, self.preview_format))
                     filename_meta = os.path.join(output_subfolder, "raw", "frame_{0:06d}.txt".format(self.frame_count))
                 else:
                     filename_fff = os.path.join(output_subfolder, "frame_{0:06d}.fff".format(self.frame_count))
                     filename_tiff = os.path.join(output_subfolder, "frame_{0:06d}.tiff".format(self.frame_count))
-                    filename_preview = os.path.join(output_subfolder, "frame_{:06d}.{}".format(self.frame_count, self.preview_format))
+                    filename_preview = os.path.join(output_subfolder,
+                                                    "frame_{:06d}.{}".format(self.frame_count, self.preview_format))
                     filename_meta = os.path.join(output_subfolder, "frame_{0:06d}.txt".format(self.frame_count))
-                
+
                 if index == 0:
                     continue
-                
+
                 # Extract next FFF frame
                 if self.use_mmap is False:
-                    chunk = seq_blob[index:index+chunksize]
+                    chunk = seq_blob[index:index + chunksize]
                 else:
                     chunk = seq_blob.read(chunksize)
-                
+
                 if i % self.step == 0:
 
                     frame = Fff(chunk)
@@ -189,11 +196,11 @@ class splitter:
                     gps_data = ','.join(str(g) for g in gps_data)
                     gpslog.write(gps_data)
                     gpslog.write('\n')
-                    
+
                     # Need FFF files to extract meta, but we do it one go afterwards
-                    if self.export_meta and self._check_overwrite(filename_fff):
-                            frame.write(filename_fff)
-                    
+                    if self.export_fff and self._check_overwrite(filename_fff):
+                        frame.write(filename_fff)
+
                     # We need at least one meta file to get the radiometric conversion coefficients
                     if meta is None and self.export_radiometric:
                         frame.write(filename_fff)
@@ -201,31 +208,30 @@ class splitter:
                         meta = self.exiftool.meta_from_file(filename_meta)
 
                     # Export raw files and/or radiometric convert them
-                    if self.export_tiff and self._check_overwrite(filename_tiff):
-                            if self.export_radiometric and meta is not None:
-                                image = frame.get_radiometric_image(meta)
-                                image += 273.15 # Convert to Kelvin
-                                image /= 0.04
-                            else:
-                                image = frame.get_image()
+                    if self.export_radiometric and meta is not None:
+                        image = frame.get_radiometric_image(meta)
+                        image += 273.15  # Convert to Kelvin
+                        image /= 0.04
+                    else:
+                        image = frame.get_image()
 
-                            self._write_tiff(filename_tiff, image)
+                    if self.export_tiff and self._check_overwrite(filename_tiff):
+                        self._write_tiff(filename_tiff, image)
 
                     # Export preview frame (crushed to 8-bit)
                     if self.export_preview and self._check_overwrite(filename_preview):
                         self._write_preview(filename_preview, image)
-            
+
                 self.frame_count += 1
-                    
-        gpslog.close()  
+
+        gpslog.close()
         return
 
 
-
 class reader:
-    
-    def __init__(self, start_index=0, step=1, width=640, height=512, file_list = None):
-                    
+
+    def __init__(self, start_index=0, step=1, width=640, height=512, file_list=None):
+
         self.width = width
         self.height = height
         self.start_index = start_index
@@ -233,7 +239,7 @@ class reader:
         self.frame_count = self.start_index
         self.use_mmap = True
         self.file_list = file_list
-            
+
         if isinstance(self.file_list, str):
             self.file_list = [self.file_list]
 
@@ -255,16 +261,16 @@ class reader:
 
         self.prev_pos = 0
         self.meta = None
-            
+
     def _get_fff_iterator(self, seq_blob):
-        
+
         magic_pattern_fff = "\x46\x46\x46\x00".encode()
 
         valid = re.compile(magic_pattern_fff)
         return valid.finditer(seq_blob)
-    
+
     def read(self):
-        
+
         index = 0;
         while (index == 0):
             match = next(self.it, None)
@@ -280,20 +286,20 @@ class reader:
             index = match.start()
             chunksize = index - self.prev_pos
             self.prev_pos = index
-       
+
         # Extract next FFF frame
         if self.use_mmap is False:
-            chunk = self.seq_blob[index:index+chunksize]
+            chunk = self.seq_blob[index:index + chunksize]
         else:
-            chunk = self.seq_blob.read(chunksize)       
+            chunk = self.seq_blob.read(chunksize)
 
         frame = Fff(chunk)
 
         gps_data = frame.get_gps()
-                                        
+
         image = frame.get_image()
-        drange = image.max()-image.min()
-        preview_data = (255.0*((image-image.min())/drange)).astype('uint8')
+        drange = image.max() - image.min()
+        preview_data = (255.0 * ((image - image.min()) / drange)).astype('uint8')
         preview_data = cv2.cvtColor(preview_data, cv2.COLOR_GRAY2RGB)
 
         return image, preview_data, gps_data
